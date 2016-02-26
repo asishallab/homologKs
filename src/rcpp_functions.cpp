e846fdde79d1a5eaeb0b007f669f109af206988b
Dr Asis Hallab <hallab@mpipz.mpg.de>
2016-02-26 18:09:48 +0100

    Added Rcpp functions and added a column of gene pair keys to the table 'chi.paranome.ks'

--- src/rcpp_functions.cpp
+++ src/rcpp_functions.cpp
@@ -1,162 +1,36 @@
-#include "rcpp_phylo_fun.h"
+#include "rcpp_functions.h"
 using namespace Rcpp ;
 
-SEXP findMatchingRow( SEXP sTable, SEXP val, SEXP colInd ){
+SEXP familyDistCpp( SEXP sGenes, SEXP sGenePairsKs, SEXP sDefDist ){
   BEGIN_RCPP
 
-    NumericMatrix table( sTable );
-    NumericVector value = NumericVector( val );
-    NumericVector columnIndex = NumericVector( colInd );
-    NumericVector clmn = table( _ , columnIndex( 0 ) );
-    int indx = clmn.size() - 1;
-    for( int i=0; i<clmn.size(); ++i ) {
-      if( clmn( i ) >= value( 0 ) ) {
-        indx = i;
-        break;
-      }
-    }
-    NumericVector rw = table( indx, _ );
-    return( wrap( rw ) );
+     CharacterVector genes(sGenes);
+     List genePairKs(sGenePairsKs);
+     CharacterVector genePairs = genePairKs.names();
+     int n( genes.size() );
+     NumericMatrix distMtrx(n,n);
+     std::vector<std::string> pair(2);
+     std::string sPair = "";
+     NumericVector defDist(sDefDist);
+     for ( int i=1; i<n; ++i ) {
+       for ( int j=0; j<i; ++j ) {
+         pair[0] = genes(i);
+         pair[1] = genes(j);
+         std::sort( pair.begin(), pair.end() );
+         sPair = pair[0] + "_" + pair[1];
+         Rcpp::Rcout << sPair << std::endl;
+         bool present =  std::find(genePairs.begin(), genePairs.end(), sPair.c_str()) != genePairs.end();
+         if ( present ) {
+           distMtrx(i, j) = as<double>( genePairKs( sPair ) );
+         } else {
+           distMtrx(i, j) = defDist(0);
+         }
+       }
+     }
 
-  END_RCPP
-}
-
-SEXP mutationProbability( SEXP compositeAnnotation, SEXP branchLength, SEXP
-    annotsMutProbTables, SEXP distanceColumnIndx ) {
-  BEGIN_RCPP
-
-    CharacterVector compAnnos( compositeAnnotation );
-    List anMutProbTbls = List( annotsMutProbTables );
-    double mutProb = 0.0;
-    for ( int i = 0; i < compAnnos.size(); ++i ) {
-      std::string singlAnno( compAnnos( i ) );
-      NumericMatrix pMutTbl = anMutProbTbls( singlAnno );
-      NumericVector pMutRow = findMatchingRow( pMutTbl, branchLength, distanceColumnIndx );
-      if ( pMutRow( 0 ) > mutProb ) {
-        mutProb = pMutRow( 0 );
-      }
-    }
-    return( wrap( NumericVector( 1, mutProb ) ) );
-
-  END_RCPP
-}
-
-SEXP conditionalProbabilityTable( SEXP branchLength, SEXP annos,
-    SEXP stringifiedAnnotations, SEXP annotsMutationProbTables, SEXP
-    mutTblLengthColIndx ) {
-  BEGIN_RCPP
-
-    List annosLst( annos );
-    CharacterVector annosAsStrs( stringifiedAnnotations );
-    NumericMatrix cpt = NumericMatrix( annosLst.size(), annosLst.size() );
-    cpt.attr( "dimnames" ) = List::create( annosAsStrs, annosAsStrs );
-
-    for ( int i = 0; i < annosLst.size(); ++i ) {
-      CharacterVector compositeAnnotation = annosLst( i );
-      double compAnnoMutProb = 1.0;
-      std::string ua = "unknown";
-      std::string caFirst = as<std::string>( compositeAnnotation( 0 ) );
-      if ( ua != caFirst ) {
-        compAnnoMutProb = ( (NumericVector) mutationProbability(
-              compositeAnnotation, branchLength, annotsMutationProbTables,
-              mutTblLengthColIndx ) )( 0 );
-      }
-      double mutToOtherAnnosProb = compAnnoMutProb / ( annosLst.size() - 1 );
-      NumericVector colmn( annosLst.size(), mutToOtherAnnosProb );
-      colmn( i ) = 1.0 - compAnnoMutProb;
-      cpt( _, i ) = colmn;
-    }
-
-    return( wrap( cpt ) );
-
-  END_RCPP
-}
-
-SEXP conditionalProbabilityTables( SEXP uniqueEdgeLengths, SEXP annos, SEXP
-    stringifiedAnnotations, SEXP annotsMutationProbTableList, SEXP
-    mutTblLengthColIndx ) {
-  BEGIN_RCPP
-
-    NumericVector edgeLengths = NumericVector( uniqueEdgeLengths );
-    CharacterVector edgeLengthsAsStrs = as<CharacterVector>( edgeLengths );
-    List cpts = List( 0 );
-
-    for ( int i = 0; i < edgeLengths.size(); ++i ) {
-      NumericVector currBranchLength( 1, edgeLengths( i ) );
-      NumericMatrix cpt = conditionalProbabilityTable( currBranchLength, annos,
-          stringifiedAnnotations, annotsMutationProbTableList,
-          mutTblLengthColIndx );
-      cpts.push_back( cpt, std::string( edgeLengthsAsStrs( i ) ) );
-    }
-
-    return( wrap( cpts ) );
-
-  END_RCPP
-}
-
-SEXP proteinsWithGOtermAnnotation( SEXP goTermAccession, SEXP
-    proteinGOannotationMatrix, SEXP proteinAccessionColIndx, SEXP goTermColInd
-    ) {
-  BEGIN_RCPP
-
-    // Initializations:
-    std::string goAcc( CharacterVector( goTermAccession )( 0 ) );
-    
-    int colProt( NumericVector( proteinAccessionColIndx )( 0 ) );
-    int colGO( NumericVector( goTermColInd )( 0 ) );
-    CharacterMatrix protGOannos( proteinGOannotationMatrix );
-    CharacterVector annotatedGOs( protGOannos( _, colGO ) );
-    CharacterVector annotatedProts( protGOannos( _, colProt ) );
-    CharacterVector matchingProts;
-
-    // Find accessions of proteins annotated with the GO term
-    // 'goTermAccession':
-    for (int i = 0; i < annotatedGOs.size(); ++i) {
-      if ( std::string( annotatedGOs( i ) ) == goAcc ) {
-        matchingProts.push_back( annotatedProts( i ) );
-      }
-    }
-
-    return( wrap( matchingProts ) );
-
-  END_RCPP
-}
-
-// Functor used for the creation of a set of vectors of std::strings and length
-// two. Used in function uniqueProteinPairs(…)
-struct Comparator {
-  bool operator()(const std::vector<std::string> & a, const std::vector<std::string> & b) {
-    const bool swapA = a[0] < a[1];
-    const std::string & al = swapA ? a[0] : a[1];
-    const std::string & ar = swapA ? a[1] : a[0];
-    const bool swapB = b[0] < b[1];
-    const std::string & bl = swapB ? b[0] : b[1];
-    const std::string & br = swapB ? b[1] : b[0];
-    return al < bl || (al == bl && ar < br);
-  }
-};
-
-// Extracts the subset of unique protein pairs from argument 'proteinPairsTbl'.
-// Here each row is considered a symmetric pair of strings. Uses above functor
-// to construct a std::set.
-SEXP uniqueProteinPairs( SEXP proteinPairsTbl, SEXP pairFirstMemberColIndex,
-    SEXP pairSecondMemberColIndex ) {
-  BEGIN_RCPP
-
-    StringMatrix tbl( proteinPairsTbl );
-    int colA( NumericVector( pairFirstMemberColIndex )( 0 ) );
-    int colB( NumericVector( pairSecondMemberColIndex )( 0 ) );
-    std::set<std::vector<std::string>, Comparator> prs;
-
-    for ( int i=0; i<tbl.nrow(); ++i ) {
-      CharacterVector r( tbl( i, _ ) );
-      std::vector< std::string > p;
-      p.push_back( std::string( r( colA ) ) );
-      p.push_back( std::string( r( colB ) ) );
-      prs.insert( p );
-    }
-
-    return( wrap( prs ) );
+     rownames(distMtrx) = genes;
+     colnames(distMtrx) = genes;
+     return( wrap( distMtrx ) );
 
   END_RCPP
 }
--- src/rcpp_functions.h
+++ src/rcpp_functions.h
@@ -1,52 +1,29 @@
-#ifndef _PhyloFun_RCPP_PHYLO_FUN_H
-#define _PhyloFun_RCPP_PHYLO_FUN_H
+//Includes/namespaces
 
 #include <Rcpp.h>
 
 using namespace Rcpp ;
 
-/*
- * Function returns the row of matrix 'table' in which column 'colInd's value
- * is >= argument 'value'. Exported to be callable from R.
- */
-RcppExport SEXP findMatchingRow( SEXP sTable, SEXP val, SEXP colInd );
-
-/*
- * Finds the maximum mutation probability for any atomic annotation in argument
- * 'compositeAnnotation'. Exported to be callable from R.
- */
-RcppExport SEXP mutationProbability( SEXP compositeAnnotation, SEXP
-    branchLength, SEXP annotsMutProbTables, SEXP distanceColumnIndx );
-
-/*
- * Generates the conditional probability table which holds the mutation
- * probability for each composite annotation in argument 'annos' to each
- * composite annotation. The table is returned as a NumericMatrix with row and
- * column names being the respective stringified annotations as in argument
- * 'stringifiedAnnotations'. Exported to be callable from R.
- */
-RcppExport SEXP conditionalProbabilityTable( SEXP branchLength, SEXP annos,
-    SEXP stringifiedAnnotations, SEXP annotsMutationProbTables, SEXP
-    mutTblLengthColIndx );
-
-/*
- * Generates one mutation probability table for each unique phylogenetic branch
- * length in argument 'uniqueEdgeLengths'. Invokes function
- * conditionalProbabilityTable with each such branch length. Returns a named
- * list of numeric matrices, in which the names are the unique branch lengths
- * as strings. Exported to be callable from R.
- */
-RcppExport SEXP conditionalProbabilityTables( SEXP uniqueEdgeLengths, SEXP
-    annos, SEXP stringifiedAnnotations, SEXP annotsMutationProbTableList, SEXP
-    mutTblLengthColIndx );
-
-/*
- * Extracts the subset of unique protein pairs from argument 'proteinPairsTbl',
- * of which each row is interpeted as an unordered pair. In each row the
- * members of the respective protein pair are identified using argument column
- * indices 'pairFirstMemberColIndex' and 'pairSecondMemberColIndex'.
- */
-RcppExport SEXP uniqueProteinPairs( SEXP proteinPairsTbl, SEXP
-    pairFirstMemberColIndex, SEXP pairSecondMemberColIndex );
-
-#endif
+//' @title
+//' familyDistCpp
+//' @description
+//' Generates a distance matrix for each pair of genes generated from 'sGenes'
+//' using the distances (Ks) values held in 'sGenePairsKs'. If a pair is not
+//' found in 'sGenePairsKs' the default distance (Ks) 'sDefDist' is used instead.
+//' 
+//' @param sGenes a character vector of gene identifiers 
+//' 
+//' @param sGenePairsKs a List in which the keys are gene pairs and the values
+//' are their pre-computed distances (Ks). Gene pairs are encoded as their
+//' concatonated identifiers in alphabetical order separated bu "_", e.g. "A_B"
+//' and not "B_A".
+//'
+//' @param sDefDist a numeric vector holding a single value which is to be used
+//' as the default distance for pairs for which no pre-computed distance can be
+//' found in 'genePairsKs'. This should be a very large value, because
+//' pre-computed distances are lacking for pairs with no significant sequence
+//' similarity.
+//' 
+//' @export
+// [[Rcpp::export]]
+RcppExport SEXP familyDistCpp( SEXP sGenes, SEXP sGenePairsKs, SEXP sDefDist );
